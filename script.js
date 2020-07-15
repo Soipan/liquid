cf0947cc2a4d2090904fd80f65729ebf6e1f5ce6
Sxifty <63594302+Soipan@users.noreply.github.com>
2020-07-15 15:37:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Challenge 1: Your age in days 
+
+function ageInDays()
+{
+    var yearOfBirth = prompt('What is your year of birth amigos?');
+    var ageInDayz = (2020-yearOfBirth)*365;
+ 
+
+}
